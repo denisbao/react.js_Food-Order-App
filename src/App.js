4c276b543b9413af7d1f6496e1fdb94295db899e
Denis Bao <denis.tpd@gmail.com>
2022-05-02 15:54:52 -0300

    HeaderCartButton abrindo o Cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import { Header } from './componets/Layout/Header';
 import { Meals } from './componets/Meals/Meals';
 import { Cart } from './componets/Cart/Cart'
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false)
+
+  function showCartHandler() {
+    setCartIsShown(true)
+  }
+
+  function hideCartHandler() {
+    setCartIsShown(false)
+  }
+
   return (
     <>
-      <Cart />
-      <Header />
+    {cartIsShown && <Cart />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/componets/Layout/Header.js
+++ src/componets/Layout/Header.js
@@ -3,12 +3,12 @@ import { HeaderCartButton } from './HeaderCartButton'
 import classes from './Header.module.css'
 import mealsImage from '../../assets/meals.jpg'
 
-export function Header() {
+export function Header(props) {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       {/* The sintax used in this className is different just because the name of the CSS class has a "-" on it. */}
       <div className={classes['main-image']}>
