795b21fd1545177629abe684bc446debe1f6ba35
Denis Bao <denis.tpd@gmail.com>
2022-05-02 09:11:27 -0300

    iniciando os componentes relacionados aos Meals

---
+++ src/componets/Meals/Meals.js
@@ -0,0 +1,11 @@
+import { AvailableMeals } from "./AvailableMeals";
+import { MealsSummary } from './MealsSummary';
+
+export function Meals() {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  )
+}
