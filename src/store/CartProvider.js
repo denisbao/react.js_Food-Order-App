e82c14edc5ec599b11fe4c3e0c6d91edb5b5edc9
Denis Bao <denis.tpd@gmail.com>
2022-05-05 15:59:51 -0300

    agrupando itens iguais adicionados ao Cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,8 +9,27 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD_MEAL') {
-    const updatedItems = state.items.concat(action.item)
+    // update total price amount of the cart:
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+    // verify if the item being added already exists in the cart:
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id)
+    const existingCartItem = state.items[existingCartItemIndex]
+    let updatedItems
+
+    // if the item already exists, the item remains the same but the amount is increased:
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      }
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    }
+    // if the item is been added to the cart for the first time 
+    else {
+      updatedItems = state.items.concat(action.item)
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
