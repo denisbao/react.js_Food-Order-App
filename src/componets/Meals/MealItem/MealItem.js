f606cc72f7094a1a881069911b2ca101646a83c7
Denis Bao <denis.tpd@gmail.com>
2022-05-05 10:44:55 -0300

    adicionando itens no Cart com Reducer e Refs

--- src/componets/Meals/MealItem/MealItem.js
+++ src/componets/Meals/MealItem/MealItem.js
@@ -1,10 +1,21 @@
 import { MealItemForm } from './MealItemForm'
+import { useContext } from 'react'
+import CartContext from '../../../store/cart-context'
 
 import classes from './MealItem.module.css'
 
 export function MealItem(props) {
-
+  const cart = useContext(CartContext)
   const price = `R$ ${props.price.toFixed(2)}`
+
+  function addToCartHandler(amount) {
+    cart.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    })
+  }
 
   return (
     <li className={classes.meal}>
@@ -14,7 +25,7 @@ export function MealItem(props) {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler}/>
       </div>
     </li>
   )
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,18 +1,40 @@
+import { useReducer } from 'react'
 import CartContext from './cart-context'
 
+// REDUCER ========================
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_MEAL') {
+    const updatedItems = state.items.concat(action.item)
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+  
+  return defaultCartState
+}
+// REDUCER ========================
+
 export function CartProvider(props) {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
-  function addItemToCartHandler(item) {
-
+  function addItemToCartHandler(handledItem) {
+    dispatchCartAction({type: 'ADD_MEAL', item: handledItem})
   } 
-  function removeItemFromCartHandler(id) {
-
+  function removeItemFromCartHandler(handledId) {
+    dispatchCartAction({type: 'REMOVE_MEAL', id: handledId})
   }
 
   // Helper constant, creating the object passed to the provider
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler
   }
