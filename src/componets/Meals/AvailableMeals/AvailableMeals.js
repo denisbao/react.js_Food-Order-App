0a4b7b1edc4b10ae36e71c7763b09ac418260e4f
denisbao <denis.bao@cowmed.com.br>
2022-06-08 10:35:12 -0300

    218 - Handling Http Errors

--- src/componets/Meals/AvailableMeals/AvailableMeals.js
+++ src/componets/Meals/AvailableMeals/AvailableMeals.js
@@ -8,11 +8,16 @@ import classes from './AvailableMeals.module.css'
 export function AvailableMeals() {
   const [meals, setMeals] = useState([])
   const [isLoaging, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState(null)
 
   useEffect(() => {
 
     async function fetchMeals() {
       const response = await fetch('https://food-order-app-f43e9-default-rtdb.firebaseio.com/meals.json')
+      if (!response.ok) {
+        throw new Error()
+      }
+
       const responseData = await response.json()
 
       const loadedMeals = []
@@ -26,7 +31,13 @@ export function AvailableMeals() {
       }
       setMeals(loadedMeals)
     }
-    fetchMeals()
+
+    // this fucntion returns a promisse and we can't use await here becouse of the useEffect
+    fetchMeals().catch(error => {
+      setIsLoading(false)
+      setHttpError('Sorry... Something went wrong.')
+    })
+
     setIsLoading(false)
   }, []);
 
@@ -34,6 +45,14 @@ export function AvailableMeals() {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading Meals...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
