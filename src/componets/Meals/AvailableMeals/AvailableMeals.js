3cd4bb61308cf1d82b29746f827d16fc55e8cfbe
denisbao <denis.bao@cowmed.com.br>
2022-06-08 10:04:43 -0300

    216 - Fetching Meals via HTTP from Firebase

--- src/componets/Meals/AvailableMeals/AvailableMeals.js
+++ src/componets/Meals/AvailableMeals/AvailableMeals.js
@@ -1,14 +1,34 @@
+import { useEffect, useState } from 'react';
 import { Card } from '../../UI/Card/Card'
 import { MealItem } from '../MealItem/MealItem';
-
-import { DUMMY_MEALS } from '../../../data/dummy-meals'
 
 import classes from './AvailableMeals.module.css'
 
 
 export function AvailableMeals() {
+  const [meals, setMeals] = useState([])
 
-  const mealsList = DUMMY_MEALS.map(meal => 
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch('https://food-order-app-f43e9-default-rtdb.firebaseio.com/meals.json')
+      const responseData = await response.json()
+
+      const loadedMeals = []
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        })
+      }
+      setMeals(loadedMeals)
+    }
+    fetchMeals()
+  }, []);
+
+
+  const mealsList = meals.map(meal => 
     <MealItem 
       key={meal.id}
       id={meal.id}
